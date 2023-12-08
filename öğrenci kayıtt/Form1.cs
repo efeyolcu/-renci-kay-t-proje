f10cc3223a469a4f8202e31c93339f6d93ba0800
efe <efe@DESKTOP-2JR0BBO>
2023-12-08 20:11:03 +0300

    Add project files.

---
+++ öğrenci kayıtt/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace öğrenci_kayıtt
+{
+    public partial class Form1 : Form
+    {
+        
+        
+         
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        SqlConnection connection = new SqlConnection("Data Source=DESKTOP-2JR0BBO\\SQLEXPRESS;Initial Catalog=okulveritabanı;Integrated Security=True");
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("insert into Table_1(kullanıcıadı,sifre,ad,soyad,numara) values(@1,@2,@3,@4,@5)" ,connection);
+            cmd.Parameters.AddWithValue("@1", kadi.Text);
+            cmd.Parameters.AddWithValue("@2", sifre.Text);
+            cmd.Parameters.AddWithValue("@3", textbox1.Text);
+            cmd.Parameters.AddWithValue("@4", textbox2.Text);
+            cmd.Parameters.AddWithValue("@5", textbox3.Text);
+            cmd.EndExecuteNonQuery();
+            connection.Close();
+            string kullanıcıadı = kadi.Text;
+          
+          
+            if (kadi.Text == "efe" && sifre.Text=="1212")
+            {
+                kayıtekle ekle = new kayıtekle();
+                ekle.Show();
+                this.Hide();
+            }
+           
+            else
+            {
+                MessageBox.Show("hatalı girdiniz!!!");
+                kadi.Clear();
+                sifre.Clear();
+                kadi.Focus();
+            }
+            SqlCommand command = new SqlCommand("Select *fromkayıt", connection);
+            SqlDataReader reader = command.ExecuteReader();
+        }
+    }
+}
---
+++ öğrenci kayıtt/kayıtekle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace öğrenci_kayıtt
+{
+    public partial class kayıtekle : Form
+    {
+        public kayıtekle()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("insert into Table_1(kullanıcıadı,sifre,ad,soyad,numara) values(@1,@2,@3,@4,@5)", connection);
+            cmd.Parameters.AddWithValue("@1", kadi.Text);
+            cmd.Parameters.AddWithValue("@2", sifre.Text);
+            cmd.Parameters.AddWithValue("@3", textbox1.Text);
+            cmd.Parameters.AddWithValue("@4", textbox2.Text);
+            cmd.Parameters.AddWithValue("@5", textbox3.Text);
+            cmd.EndExecuteNonQuery();
+            connection.Close();
+            string ad=textBox1.Text;
+            string soyad=textBox2.Text;
+            
+
+            string num=textBox3.Text;
+            int numara=int.Parse(num);
+
+            label4.Text = "kayıt başarılı";
+
+         
+        }
+
+        private void gor_Click(object sender, EventArgs e)
+        {
+            string ad = textBox1.Text;
+            string soyad = textBox2.Text;
+
+
+            string num = textBox3.Text;
+            int numara = int.Parse(num);
+
+            label4.Text = "kayıt görüntüleniyor";
+
+
+            textBox4.Text=(ad+" " + soyad+" " + numara);
+
+
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kayıtekle_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
